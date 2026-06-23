92a76ca850ae0ccdafec1d629386827e7a453fb8
ashis373 <ashis.rout@hexalearn.co.in>
2026-06-23 16:55:12 +0530

    all unused file will be delted form api folder

--- check_roles.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'api/db.php';
-$stmt = $conn->query("SELECT * FROM system_roles");
-$roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode(["status" => "success", "data" => $roles]);
-?>
