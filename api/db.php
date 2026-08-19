7f20bb86efba1fd96e67a2ec79fce593f4ec5bc7
ashis373 <ashis.rout@hexalearn.co.in>
2026-08-19 13:00:31 +0530

    For now i wii change the ui part ok dasbore ui not looking good so i  wii change the ui make it somthik difrent way

--- api/db.php
+++ api/db.php
@@ -1,4 +1,6 @@
 <?php
+// local server credentials
+
 $host = "localhost";
 $dbname = "cims";
 $username = "root";
@@ -9,8 +11,34 @@ date_default_timezone_set('Asia/Kolkata');
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Database Connected Successfully";
+    
 } catch (PDOException $e) {
     die(json_encode(["error" => "Connection Failed: " . $e->getMessage()]));
 }
+
+
+
+
+
+
+// Server credentials for production server
+
+// $host = "localhost";
+// $dbname = "u413479707_demohub";
+// $username = "u413479707_hexadev";
+// $password = "hexadev$0014B";
+
+// try {
+//     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+// } catch (PDOException $e) {
+//     die(json_encode(["error" => "Connection Failed: " . $e->getMessage()]));
+// }
+
+
 ?>
+
+
+
+
--- check_db2.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'api/db.php';
-$stmt = $conn->query('SELECT u.email, u.role_id, r.role_name FROM system_users u LEFT JOIN system_roles r ON u.role_id = r.id');
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
